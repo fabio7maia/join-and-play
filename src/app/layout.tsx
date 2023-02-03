dbd48f40a51a8854ebb9d08e603f2889496e94a9
Fábio Maia <fabio.maia@itsector.pt>
2023-02-03 22:29:38 +0000

    feat: added google adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import '../styles/globals.css';
 import Footer from '@components/footer';
 import NavBar from '@components/navBar.client';
 
-export default async function RootLayout({ children, ...props }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
 	// const user = rsc.whoami.use();
 
 	return (
@@ -16,6 +16,11 @@ export default async function RootLayout({ children, ...props }: { children: Rea
 					<title>Join and Play</title>
 					<link rel="icon" href="/favicon.ico" />
 					<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+					<script
+						async
+						src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7421026220240639"
+						crossOrigin="anonymous"
+					></script>
 				</head>
 				<body>
 					<div className="flex flex-col h-screen">
